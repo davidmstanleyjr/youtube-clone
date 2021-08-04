a09e832e8b0ed739e080fa18f53231da3bcc899a
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-04 00:23:09 -0400

    setting things up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+import youtube from './api/youtube';
+
+class App extends React.Component {
+	render() {
+		return (
+		<Grid container spacing={16}>
+			
+		</Grid>
+		)
+	}
+}
+
+export default App;
